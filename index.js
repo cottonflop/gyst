52f201df641c255a7a96a7a82ac5aa07984a875c
Betsy Fox <betsy.fox@sony.com>
2017-09-12 17:03:43 -0700

    catchup (#13)
    * parser in prog
    
    * more parse refactoring
    
    * cleanup
    
    * more cleanup
    
    * massive parsing refactor in progress
    
    * conv symbol types to something closer to an enum
    
    * more parsing basics
    
    * more benchmarks determine this is the best way so far, moving onto the practical stuff now
    
    * add catalog
    
    * making simple assignment default
    
    * on the way to simplicity?
    
    * complete rewrite of lexer using npm lex package
    
    * cleanup
    
    * cleanup
    
    * better terminal output, slightly easier to read. WIP.
    
    * better error handling because of better token handling
    
    * almost executable! WIP
    
    * rewriting lexer
    
    * more new lexer work
    
    * need to refactor catalog and proc storage, too messy
    
    * still wip
    
    * cleanup
    
    * refactoring proc calls back into the main loop
    
    * forgot to add file to last commit
    
    * yet more refactoring oh god
    
    * refactored for significant lexer changes
    
    * fallen far behind...
    
    * fallen behind
    
    * catchup

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
 
-var chalk = require('chalk');
-var clear = require('clear');
-// var cli         = require('cli');
-var figlet = require('figlet');
-var fs = require('fs');
-var files = require('./lib/files.js');
-// var catalog   = require('./lib/catalog.js')
+let chalk = require('chalk');
+let clear = require('clear');
+// let cli         = require('cli');
+let figlet = require('figlet');
+let fs = require('fs');
+let files = require('./lib/files.js');
+let registry = require('./lib/registry.js')();
 
-var {	lexer } = require('bluesocks');
-var rules = require('./lib/rules');
-var { heading, log } = require('./lib/common.js')
+let {	lexer } = require('bluesocks');
+let rules = require('./lib/rules');
+let { heading, log } = require('./lib/common.js');
 
-// var { procs, pre, post }
+let world = {};
+
+// let { functions, pre, post }
 
 args = process.argv;
 args.shift();
@@ -20,9 +22,9 @@ args.shift();
 
 
 
-var execution_list = [];
+let execution_list = [];
 
-splash = function() {
+let splash = function() {
 	clear();
 	console.log(
 		chalk.green(figlet.textSync('Gyst', {
@@ -35,51 +37,57 @@ splash = function() {
 }
 
 
-var procs = new Map();
-var call_proc = function(token) {
-	let proc_names = procs.keys();
-	let proc_name = proc_names.next();
-	while (!proc_name.done) {}
-}
-
-
-var error = function(token, msg) {
+let error = function(token, msg) {
 	console.log(chalk.red(`Error at ${token.value.src}:${token.value.row}:${token.value.col}, ${msg}\n`));
 }
 
 
-var execute = function(callstack) {
-	switch (typeof callstack) {
-		case "object":
-			callstack.forEach(function(x) {
-				console.log(`EXECUTING ${x.data} FROM ${x.src}:${x.row}:${x.col}`);
-				if (x.type == 'CALLPROC') execute(x.data);
-				else execute(procs.get(x.data));
-			});
-			break;
-		case "string":
-			execute(procs.get(callstack));
-			break;
-		case "function":
-			console.log(callstack);
-		default:
-			// console.log(`We hit something that wasn't a string or an object/array- typeof is "${typeof callstack}" and the value is "${callstack}"`);
-			break;
+let execute = function(callstack) {
+	if (callstack instanceof Array) {
+		console.log("callstack is totes an array")
+		for (x of callstack) {
+			execute(x.data);
+		}
+	} else if (callstack instanceof Function) {
+		callstack.apply(world);
+		console.log("callstack is totes a function");
+	} else if (typeof callstack == "string") {
+		console.log("callstack is totes a string" + callstack)
+	} else {
+		console.log("Well, callstack was neither an array, a function, or a string...: " + callstack);
 	}
+	// console.log(callstack);
+	// switch (typeof callstack) {
+	// 	case "object":
+	// 		callstack.forEach(function(x) {
+	// 			console.log(`EXECUTING ${x.data} FROM ${x.src}:${x.row}:${x.col}`);
+	// 			if (x.type == 'CALLPROC') execute(x.data);
+	// 			else execute(registry.get_function(x.data));
+	// 		});
+	// 		break;
+	// 	case "string":
+	// 		execute(registry.get_function(callstack));
+	// 		break;
+	// 	case "function":
+	// 		console.log(callstack);
+	// 	default:
+	// 		// console.log(`We hit something that wasn't a string or an object/array- typeof is "${typeof callstack}" and the value is "${callstack}"`);
+	// 		break;
+	// }
 	// if (typeof callstack == "object") {
 	// 	callstack.forEach(function(x) {
 	// 		 console.log("EXECUTING", x.data);
 	// 		if (x.type == 'CALLPROC')
 	// 			execute(x.data);
-	// 		// console.log(procs.get(x.data));
+	// 		// console.log(functions.get(x.data));
 	// 	});
 	// } else {
-	// 	// console.log(procs.get(x.data));
+	// 	// console.log(functions.get(x.data));
 	// }
 }
 
 
-var parse_feature = function(lexer) {
+let parse_feature = function(lexer) {
 	let token = lexer.next();
 	while (!token.done && token.value.type != "ENDBLOCK") {
 		token = lexer.next();
@@ -87,7 +95,7 @@ var parse_feature = function(lexer) {
 }
 
 
-var parse_procedure = function(lexer) {
+let parse_procedure = function(lexer) {
 	let token = lexer.next();
 	let stack = [];
 	while (!token.done && token.value.type != "ENDBLOCK") {
@@ -107,27 +115,27 @@ var parse_procedure = function(lexer) {
 	return stack;
 }
 
+let wrap_function = function(s) {
+	return `(function(){${s}})`;
+}
 
-var parse_native = function(lexer) {
+
+let parse_native = function(lexer) {
 	let token = lexer.next();
+	let out = [];
 	while (!token.done && token.value.type != "ENDBLOCK") {
-		console.log(`"${token.value.data}"`);
-		// eval();
+		out.push(token.value.data)
 		token = lexer.next();
 	}
-	lexer.next("procedure");
-	return "";
+	return wrap_function(out.join("\n"));
 }
 
 
-var eat_keyword = function(s) {
-	return s.substr(s.search(" ") + 1);
-}
 
-
-var parse = function(lexer) {
+let parse = function(lexer) {
 	let token = lexer.next();
 	while (!token.done) {
+		// console.log(token.value.data);
 		switch (token.value.type) {
 			case "UNKNOWN":
 				error(token, `Undefined command: "${token.value.data}"`)
@@ -138,7 +146,7 @@ var parse = function(lexer) {
 			case "SCENARIO":
 				execution_list.push(token.value);
 			case "PROCEDURE":
-				procs.set(token.value.data, parse_procedure(lexer));
+				registry.register(token.value.data, parse_procedure(lexer));
 				break;
 			case "WHITESPACE":
 			case "NEWLINE":
@@ -154,8 +162,14 @@ var parse = function(lexer) {
 args.forEach(function(val, index) {
 	specFiles = files.ls(`${process.cwd()}/${val}`);
 	specFile = specFiles.next();
+	let i = 0;
 	while (!specFile.done) {
-		if (specFile.value.toLowerCase().endsWith(".specs")) {
+		console.log(++i);
+		if (specFile.value.toLowerCase().endsWith("world.js")) {
+			console.log("found a world file");
+			world = require(specFile.value);
+			// console.log(world);
+		} else if (specFile.value.toLowerCase().endsWith(".specs")) {
 			heading(`Processing ${specFile.value}...`);
 			let data = files.read(specFile.value);
 			lex = lexer(data, specFile.value, rules);
@@ -164,8 +178,7 @@ args.forEach(function(val, index) {
 		specFile = specFiles.next();
 	}
 
-	let keys = procs.keys();
-	let key = keys.next();
+	// registry.dump();
 
 	execute(execution_list);
 });
